abceb15a9acffb3b22d20c74ba65985529775dcf
unknown <barteku12@gmail.com>
2018-12-19 13:20:35 +0100

    add static homepage view

--- raports_app/raports_app/views.py
+++ raports_app/raports_app/views.py
@@ -1,11 +1,25 @@
-from django.http import HttpResponse
-from django.shortcuts import render
+
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, redirect
 
 def home(request):
-    return HttpResponse("<h1> Witaj na stronie głównej generatora raportów</h1>")
+    return render(request, 'home.html')
 
 def sign_up(request):
-    return render(request, 'signup.html')
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)
+            login(request, user)
+            return redirect('home')
+    else:
+        form = UserCreationForm()
+    return render(request, 'signup.html', {'form': form})
+
 
 def login(request):
     return render(request, 'login.html')

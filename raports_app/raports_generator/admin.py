c1c716111616c4ebb9bc6885dd0d5bd1d45f3825
Filip <filiprebizant@gmail.com>
2018-12-27 15:32:32 +0100

    Create models

--- raports_app/raports_generator/admin.py
+++ raports_app/raports_generator/admin.py
@@ -1,3 +1,13 @@
 
 # Register your models here.
 
+from django.contrib import admin
+
+from .models import User, Product, Client, Invoice, Report, Purchase
+
+admin.site.register(User)
+admin.site.register(Product)
+admin.site.register(Client)
+admin.site.register(Invoice)
+admin.site.register(Report)
+admin.site.register(Purchase)
--- raports_app/raports_generator/models.py
+++ raports_app/raports_generator/models.py
@@ -1,28 +1,12 @@
 from django.db import models
 
-# Create your models here.
-class Report(models.Model):
-    report_id = models.IntegerField(null=True)
-    invoice_id = models.IntegerField(null=True)
-    date_of_generation = models.DateField(null=True)
-
-    def getReport(self):
-        return
-
-    def generateChart(self):
-        return
-
-    def generateTable(self):
-        return
-
-
 
 class Client(models.Model):
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
+    email = models.CharField(max_length=50)
     phone_number = models.CharField(max_length=30)
     NIP_number = models.CharField(max_length=30)
-    email = models.CharField(max_length=50)
 
     def addClient(self):
         return
@@ -35,34 +19,16 @@ class Product(models.Model):
     name = models.CharField(max_length=100)
 
     def addProduct(self):
-
         return
 
     def deleteProduct(self):
         return
 
 
-class Purchase(models.Model):
-    product = models.ForeignKey(Product, on_delete=models.CASCADE)
-    quantity = models.IntegerField(null=True)
-    purchase_value = models.FloatField(null=True)
-
-    def addPurchase(self):
-        return
-
-
-    def deletePurchase(self):
-        return
-
-
-class Invoice(models.Model):
-    purchase = models.ForeignKey(Purchase, on_delete = models.CASCADE)
-
-
 class User(models.Model):
-    login = models.CharField(max_length=20)
-    email = models.CharField(max_length = 50)
+    email = models.CharField(max_length=50)
     password = models.CharField(max_length=20)
+    login = models.CharField(max_length=20)
 
     def login(self):
         return
@@ -75,16 +41,42 @@ class User(models.Model):
 
 
 class Report(models.Model):
-    user = models.ForeignKey(User, on_delete = models.CASCADE)
-    invoice = models.ForeignKey(Invoice, on_delete = models.CASCADE)
-    start_date = models.DateField(null=True)
-    end_date = models.DateField(null=True)
+    start_date = models.DateTimeField()
+    end_date = models.DateTimeField()
 
     def addReport(self):
         return
 
-    def editChart(self):
+    def editReport(self):
         return
 
-    def deleteTable(self):
+    def deleteReport(self):
+        return
+
+    def getReport(self):
+        return
+
+    def generateChart(self):
+        return
+
+    def generateTable(self):
+        return
+
+
+class Invoice(models.Model):
+    client = models.ForeignKey(Client)
+    date_of_issue = models.DateField()
+    reports = models.ManyToManyField(Report, null=True)
+
+
+class InvoiceItem(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.IntegerField()
+    purchase_value = models.FloatField()
+    invoice = models.ForeignKey(Invoice, on_delete=models.CASCADE)
+
+    def addPurchase(self):
+        return
+
+    def deletePurchase(self):
         return

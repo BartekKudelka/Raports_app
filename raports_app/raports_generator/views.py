bb9680f1bbe8cf40c40ff2d25d90e881e2b74a17
Filip <filiprebizant@gmail.com>
2019-01-05 17:18:41 +0100

    Add visual reports

--- raports_app/raports_generator/views.py
+++ raports_app/raports_generator/views.py
@@ -46,111 +46,92 @@ def show_text_report(request, id):
 def show_visual_report(request, id):
     report = Report.objects.get(id=id)
 
-    grouped = report.invoices.all() \
+    query_items = report.invoices.all() \
         .annotate(month=TruncMonth('date_of_issue')) \
         .values('month') \
         .annotate(c=Count('id'), s=Sum('invoice_item__quantity')) \
         .values('month', 'c', 's', 'invoice_item__product__name')
 
-    mts = report.invoices.all() \
+    months_query = report.invoices.all() \
         .annotate(month=TruncMonth('date_of_issue')) \
         .values('month') \
         .annotate(c=Count('id'), s=Sum('invoice_item__quantity')) \
         .values('month').distinct()
 
-    months = list()
-    obj = dict()
-    products = list()
     all_products = report.invoices.all().values('invoice_item__product__name').distinct()
 
-    # Add months
-    for month in grouped:
-        mth = month['month'].strftime('%Y-%m')
-        months.append(mth)
+    items_list = list()
+    obj = dict()
+    products = list()
+    series = []
+
+    # Add items
+    for item in query_items:
+        mth = item['month'].strftime('%Y-%m')
+        items_list.append(mth)
         obj.update({mth: {}})
 
-    results = grouped
-
-
-
+    # Add products to list
     for product in all_products:
         products.append(product['invoice_item__product__name'])
 
-    print('Products count:' + str(len(products)))
-    months_number = len(months)
-    print('montsh ' + str(len(mts)))
-    listOfLists = []
+    number_of_items = len(items_list)
+    list_of_lists = []
 
-    for x in range(len(mts)):
-        listOfLists.append(x)
-        listOfLists[x] = [0] * len(all_products)
+    for x in range(len(months_query)):
+        list_of_lists.append(x)
+        list_of_lists[x] = [0] * len(all_products)
 
-    for x in range(months_number):
-        for index, elem in enumerate(grouped):
+    for x in range(number_of_items):
+        for index, elem in enumerate(query_items):
             for key, val in obj.items():
                 if key == elem['month'].strftime('%Y-%m'):
                     obj[key].update({elem['invoice_item__product__name']: elem['s']})
 
-    print(listOfLists)
+    # Add sum of products to given month
+    for list_index, list_val in enumerate(list_of_lists):
+        for index, query_item in enumerate(query_items):
+            for dict_date, dict_items in obj.items():
+                for dict_product, dict_sum in dict_items.items():
+                    for product_index, product in enumerate(products):
+                        if query_item['invoice_item__product__name'] == dict_product:
+                            if query_item['month'].strftime('%Y-%m') == dict_date:
+                                if query_item['invoice_item__product__name'] == product:
+                                    if months_query[list_index]['month'].strftime('%Y-%m') == query_item['month'].strftime('%Y-%m'):
+                                        list_of_lists[list_index][product_index] = dict_sum
 
-    #działa
-    # for index, product in enumerate(grouped):
-    #     for key, val in obj.items():
-    #         for k, v in val.items():
-    #             for i, p in enumerate(products):
-    #                 if product['invoice_item__product__name'] == k and product['month'].strftime('%Y-%m') == key and product['invoice_item__product__name'] == p:
-    #                     quantity[i] = v
-    #                 elif product['invoice_item__product__name'] == k and not product['month'].strftime('%Y-%m') == key and product['invoice_item__product__name'] == p:
-    #                     quantity2[i] = v
 
+    colors = [
+        'green',
+        'blue',
+        'black',
+        'orange',
+        'purple',
+        'grey',
+        '#00f5f5',
+        '#00ff22',
+        'yellow',
+        'red',
+        'brown',
+        '#ff55ff',
+    ]
 
-    for ind, val in enumerate(listOfLists):
-        for index, product in enumerate(grouped):
-            for key, val in obj.items():
-                for k, v in val.items():
-                    for i, p in enumerate(products):
-                        if product['invoice_item__product__name'] == k and product['month'].strftime('%Y-%m') == key and product['invoice_item__product__name'] == p:
-                            listOfLists[0][i] = v
-                        elif product['invoice_item__product__name'] == k and not product['month'].strftime('%Y-%m') == key and product['invoice_item__product__name'] == p:
-                            listOfLists[1][i] = v
-
-    #TODO: dodawanie dynamicznie list
-
-    # print('Dictionary')
-    # print(obj)
-    # print('quantity1')
-    # print(quantity)
-    # print('quantity2')
-    # print(quantity2)
-
-    series = []
-    print(listOfLists)
-    for index, elem in enumerate(listOfLists):
-        print('series')
+    for index, elem in enumerate(list_of_lists):
         series.append({
-            'name': mts[index]['month'].strftime('%Y-%m'),
+            'name': months_query[index]['month'].strftime('%Y-%m'),
             'data': elem,
-            'color': 'green'
+            'color': colors[index]
         })
 
-
-    survived_series = {
-        'name': months[0],
-        'data': listOfLists[0],
-        'color': 'green'
-    }
-
-    not_survived_series = {
-        'name': months[1],
-        'data': listOfLists[1],
-        'color': 'red'
-    }
+    if len(series) > 0:
+        info = 'Data of sales from ' + report.start_date.strftime('%Y-%m-%d') + ' to ' + report.end_date.strftime('%Y-%m-%d')
+    else:
+        info = 'There was no sales from ' + report.start_date.strftime('%Y-%m-%d') + ' to ' + report.end_date.strftime('%Y-%m-%d')
 
     chart = {
         'chart': {'type': 'column'},
-        'title': {'text': 'Titanic Survivors by Ticket Class'},
+        'title': {'text': info},
         'xAxis': {'categories': products},
-        # 'series': [survived_series, not_survived_series]
         'series': series
     }
 
@@ -158,11 +139,5 @@ def show_visual_report(request, id):
 
     return render(request, 'visual_report.html', {
         'chart': chart,
-        'obj': results,
-        'report': report,
-        'products': products
+        'report': report
     })
-
-
-
-

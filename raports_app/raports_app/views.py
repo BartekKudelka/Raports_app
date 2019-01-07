868914ab5e7173c10a0279e0468025effe5f9905
Filip <filiprebizant@gmail.com>
2019-01-07 20:46:41 +0100

    Add views

--- raports_app/raports_app/views.py
+++ raports_app/raports_app/views.py
@@ -1,31 +1,21 @@
-
 from django.contrib.auth import login, authenticate
 from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import render, redirect
 from raports_app.polls.models import User
 
+
 def home(request):
-    users = User.objects.all()
-    args = {'users': users}
-    return render(request, 'home.html', args)
+    return render(request, 'home.html')
+
 
 def sign_up(request):
-    if request.method == 'POST':
-        form = UserCreationForm(request.POST)
-        if form.is_valid():
-            form.save()
-            username = form.cleaned_data.get('username')
-            raw_password = form.cleaned_data.get('password1')
-            user = authenticate(username=username, password=raw_password)
-            login(request, user)
-            return redirect('home')
-    else:
-        form = UserCreationForm()
+    form = UserCreationForm()
     return render(request, 'signup.html', {'form': form})
 
 
 def login(request):
     return render(request, 'login.html')
+
 
 def logout(request):
     return render(request, 'logout.html')

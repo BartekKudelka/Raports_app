07bc6bdebc39a9d506ef5ade90a227d4ae1a5a6d
filiprebizant <filiprebizant@gmail.com>
2019-01-09 00:04:19 +0100

    Add tests

--- raports_app/raports_generator/tests.py
+++ raports_app/raports_generator/tests.py
@@ -9,6 +9,9 @@ from django.contrib.auth import login as auth_login, authenticate
 
 class ViewsTest(TestCase):
 
+    def setUp(self):
+        user = User.objects.create_user('temporary', 'temporary@gmail.com', 'temporary')
+
     def test_report_nopermision(self):
         response = self.client.get('/raports_generator/reports/', follow=True)
         self.assertEqual(response.status_code, 200)
@@ -37,21 +40,3 @@ class ViewsTest(TestCase):
         self.assertEqual(status_code, 302)
         self.assertEqual(last_url, '/login/?next=/raports_generator/show_visual_report/1/')
 
-
-
-    def test_visual_report_with_permision(self):
-        response = self.client.get('/raports_generator/show_visual_report/1/', follow=True)
-        login = self.client.login(username='testuser1', password='1X<ISRUkw+tuK')
-        response = self.client.get(reverse('create_report'))
-
-        # Check our user is logged in
-        self.assertEqual(str(response.context['user']), 'testuser1')
-        # Check that we got a response "success"
-
-
-        self.assertEqual(response.status_code, 200)
-        user = authenticate(username='admin2', password='zaq1@WSX')
-        print(user)
-        last_url, status_code = response.redirect_chain[-1]
-        self.assertEqual(status_code, 302)
-        self.assertEqual(last_url, '/login/?next=/raports_generator/show_visual_report/1/')

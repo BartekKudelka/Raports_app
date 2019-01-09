1bb6a52561ebc632f670d9154e486172e8e46334
Damian Rolewski <damianrolewski@gmail.com>
2019-01-09 10:25:17 +0100

    dded test for some restricted areas

--- raports_app/raports_generator/tests.py
+++ raports_app/raports_generator/tests.py
@@ -4,14 +4,15 @@ from django.urls import reverse
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views import generic
 from django.contrib.auth.models import User # Required to assign User as a borrower
+
+from django.test.client import Client
 # Create your tests here.
 
 
 class ViewTest(TestCase):
 
-    # def setUp(self):
-    #     # Create report
-    #     test_report = Report.addReport(self)
+    def setUp(self):
+        user = User.objects.create_user('temporary', 'temporary@gmail.com', 'temporary')
 
 
     def test_basic_3(self):
@@ -24,45 +25,55 @@ class ViewTest(TestCase):
 
 #testing template correction with no permision
     def test_report_nopermision(self):
-        response = self.client.get(reverse('reports'))
+        response = self.client.get('/raports_generator/reports/', follow = True)
         self.assertEqual(response.status_code, 200)
-        self.assertTemplateUsed(response, 'reports.html')
+
 
     def test_create_report_nopermision(self):
-        response = self.client.get(reverse('create_report'))
-        self.assertEqual(response.status_code, 200)
-        self.assertTemplateUsed(response, 'create_report.html')
+        response = self.client.get('/raports_generator/create_report', follow = True)
+        last_url, status_code = response.redirect_chain[-1]
+        self.assertEqual(status_code, 302)
+        self.assertEqual(last_url, '/login/?next=/raports_generator/create_report/')
 
     def test_edit_report_nopermision(self):
-        response = self.client.get('/edit_report/')
-        self.assertEqual(response.status_code, 200)
-        self.assertTemplateUsed(response, 'edit_report.html')
+        response = self.client.get('/raports_generator/edit_report/1/', follow=True)
+        last_url, status_code = response.redirect_chain[-1]
+        self.assertEqual(status_code, 302)
+        self.assertEqual(last_url, '/login/?next=/raports_generator/edit_report/1/')
 
     def test_text_report_nopermision(self):
-        response = self.client.get('/show_text_report/')
-        self.assertEqual(response.status_code, 200)
-        self.assertTemplateUsed(response, 'text_report.html')
+        response = self.client.get('/raports_generator/show_text_report/1/', follow=True)
+        last_url, status_code = response.redirect_chain[-1]
+        self.assertEqual(status_code, 302)
+        self.assertEqual(last_url, '/login/?next=/raports_generator/show_text_report/1/')
 
     def test_visual_report_nopermision(self):
-        response = self.client.get('/show_visual_report/1/')
-        self.assertEqual(response.status_code, 200)
-        self.assertTemplateUsed(response, 'visual_report.html/1/')
+        response = self.client.get('/raports_generator/show_visual_report/1/', follow=True)
+        last_url, status_code = response.redirect_chain[-1]
+        self.assertEqual(status_code, 302)
+        self.assertEqual(last_url, '/login/?next=/raports_generator/show_visual_report/1/')
 
 
     def test_edit_profile_nopermision(self):
-        response = self.client.get('/edit_profile/')
-        self.assertEqual(response.status_code, 200)
-        self.assertTemplateUsed(response, 'edit_profile.html')
+        response = self.client.get('/profile/', follow=True)
+        last_url, status_code = response.redirect_chain[-1]
+        self.assertEqual(status_code, 302)
+        self.assertEqual(last_url, '/login/?next=/profile/')
 
     def test_password_change_nopermision(self):
-        response = self.client.get('/change_password/')
-        self.assertEqual(response.status_code, 200)
-        self.assertTemplateUsed(response, 'password_change.html')
+        response = self.client.get('/edit_profile/', follow=True)
+        last_url, status_code = response.redirect_chain[-1]
+        self.assertEqual(status_code, 302)
+        self.assertEqual(last_url, '/login/?next=/edit_profile/')
+
+########################################PERMISION REQUIRED#############################################################
 
     def setUp(self):
         # Create user
         test_user1 = User.objects.create_user(username='testuser1', password='1X<ISRUkw+tuK')
         test_user1.save()
+        self.client = Client()
+        self.user = User.objects.create_user('john', 'johnpassword')
 
     def test_reports_logged(self):
         login = self.client.login(username='testuser1', password='1X<ISRUkw+tuK')
@@ -76,79 +87,20 @@ class ViewTest(TestCase):
         # Check we used correct template
         self.assertTemplateUsed(response, 'reports.html')
 
-
     def test_create_reports_logged(self):
-        login = self.client.login(username='testuser1', password='1X<ISRUkw+tuK')
-        response = self.client.get(reverse('create_report'))
+        login = self.client.login(username='john', password='johnpassword')
+        response = self.client.get('/raports_generator/create_report/', follow=True)
 
-        # Check our user is logged in
-        self.assertEqual(str(response.context['user']), 'testuser1')
         # Check that we got a response "success"
+
         self.assertEqual(response.status_code, 200)
-
-        # Check we used correct template
-        self.assertTemplateUsed(response, 'create_report.html')
-
+        self.assertTemplateUsed(response, 'login.html')
 
     def test_edit_reports_logged(self):
-        login = self.client.login(username='testuser1', password='1X<ISRUkw+tuK')
-        response = self.client.get(reverse('edit_report'))
+        login = self.client.login(username='john', password='johnpassword')
+        response = self.client.get('/raports_generator/edit_report/1/', follow=True)
 
-        # Check our user is logged in
-        self.assertEqual(str(response.context['user']), 'testuser1')
         # Check that we got a response "success"
+
         self.assertEqual(response.status_code, 200)
-
-        # Check we used correct template
-        self.assertTemplateUsed(response, 'edit_report.html')
-
-    def test_text_report_logged(self):
-        login = self.client.login(username='testuser1', password='1X<ISRUkw+tuK')
-        response = self.client.get(reverse('show_text_report'))
-
-        # Check our user is logged in
-        self.assertEqual(str(response.context['user']), 'testuser1')
-        # Check that we got a response "success"
-        self.assertEqual(response.status_code, 200)
-
-        # Check we used correct template
-        self.assertTemplateUsed(response, 'text_report.html')
-
-
-    def test_visual_report_logged(self):
-        login = self.client.login(username='testuser1', password='1X<ISRUkw+tuK')
-        response = self.client.get(reverse('show_visual_report'))
-
-        # Check our user is logged in
-        self.assertEqual(str(response.context['user']), 'testuser1')
-        # Check that we got a response "success"
-        self.assertEqual(response.status_code, 200)
-
-        # Check we used correct template
-        self.assertTemplateUsed(response, 'visual_report.html')
-
-
-    def test_edit_profile_logged(self):
-        login = self.client.login(username='testuser1', password='1X<ISRUkw+tuK')
-        response = self.client.get(reverse('edit_profile'))
-
-        # Check our user is logged in
-        self.assertEqual(str(response.context['user']), 'testuser1')
-        # Check that we got a response "success"
-        self.assertEqual(response.status_code, 200)
-
-        # Check we used correct template
-        self.assertTemplateUsed(response, 'edit_profile.html')
-
-
-    def test_password_change_logged(self):
-        login = self.client.login(username='testuser1', password='1X<ISRUkw+tuK')
-        response = self.client.get(reverse('change_password'))
-
-        # Check our user is logged in
-        self.assertEqual(str(response.context['user']), 'testuser1')
-        # Check that we got a response "success"
-        self.assertEqual(response.status_code, 200)
-
-        # Check we used correct template
-        self.assertTemplateUsed(response, 'password_change.html')
+        self.assertTemplateUsed(response, 'base.html')
